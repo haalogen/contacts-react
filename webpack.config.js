f5c5f251cc9cf08ee19d7db5328a23549f083a53
stan <citrus_82@nextmail.ru>
2017-11-14 13:57:13 +0300

    Production build

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,9 @@ module.exports = {
   devtool: 'source-map',
 
   plugins: [
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new webpack.optimize.UglifyJsPlugin()
   ]
 };
